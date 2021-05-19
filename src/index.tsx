73482c8abe0225f11b4f8c9508224bf5f62d5c7a
João Manoel Neto <joaaomanooel@gmail.com>
2021-05-19 14:25:16 -0300

    update fonts

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,23 @@ import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import * as Updates from 'expo-updates';
 import AppLoading from 'expo-app-loading';
-import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
+import {
+  useFonts,
+  Jost_300Light,
+  Jost_400Regular,
+  Jost_500Medium,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
 
 import Navigation from '@/navigation';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Jost_400Regular, Jost_600SemiBold });
+  const [fontsLoaded] = useFonts({
+    Jost_300Light,
+    Jost_400Regular,
+    Jost_500Medium,
+    Jost_600SemiBold
+  });
 
   useEffect(() => {
     async function updateApp() {
