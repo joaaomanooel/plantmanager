1b49dd701a7e8bfbc027a4f310de89d28aeaac38
João Manoel Neto <joaaomanooel@gmail.com>
2021-05-20 19:39:26 -0300

    export new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 // -- APPEND ITEMS HERE --
+export { default as PlantCard } from './PlantCard';
+export { default as EnvironmentsButton } from './EnvironmentsButton';
+export { default as Header } from './Header';
 export { default as TouchableOpacity } from './TouchableOpacity';
 export { default as Button } from './Button';
