f0a56b31c75a49d9b96734eacd92e93a53d495d6
João Manoel Neto <joaaomanooel@gmail.com>
2021-05-21 10:47:59 -0300

    create plant select screen

--- src/screens/PlantSelect/index.tsx
+++ src/screens/PlantSelect/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
-import { navigations } from '@/constants';
-import { Header } from '@/components';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Container, SubTitle, Title, HeaderWapper } from './styles';
+import { View } from 'react-native';
+
+import { EnvironmentsButton, Header, PlantCard } from '@/components';
+import { environments as environmentsService, plants as plantService } from '@/services';
+import { IEnvironments, IPlants } from '@/interfaces';
+
+import {
+  Container,
+  EnvironmentsList,
+  HeaderWapper,
+  PlantsList,
+  PlantsListWapper,
+  SubTitle,
+  Title
+} from './styles';
 
 export default () => {
   const { navigate } = useNavigation();
+  const [environments, setEnvironments] = useState<IEnvironments[]>([]);
+  const [plants, setPlants] = useState<IPlants[]>([]);
+
+  useEffect(() => {
+    async function fetchEvironments() {
+      const { data } = await environmentsService.getAll({});
+      setEnvironments([{ key: 'all', title: 'Todos' }, ...data]);
+    }
+    fetchEvironments();
+  }, []);
+
+  useEffect(() => {
+    async function fetchPlants() {
+      const { data } = await plantService.getAll({});
+      setPlants(data);
+    }
+    fetchPlants();
+  }, []);
 
   return (
     <Container>
@@ -14,6 +44,24 @@ export default () => {
         <Title>Em qual hambiente</Title>
         <SubTitle>você quer colocar sua planta?</SubTitle>
       </HeaderWapper>
+      <View>
+        <EnvironmentsList
+          renderItem={({ item }) => <EnvironmentsButton text={item.title} />}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.key}
+          data={environments}
+          horizontal={true}
+        />
+      </View>
+      <PlantsListWapper>
+        <PlantsList
+          renderItem={({ item }) => <PlantCard key={item.id} data={item} />}
+          keyExtractor={(item) => `${item.id}-${item.name}`}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          data={plants}
+        />
+      </PlantsListWapper>
     </Container>
   );
 };
--- src/screens/PlantSelect/styles.ts
+++ src/screens/PlantSelect/styles.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components/native';
 import { colors, layout, fonts } from '@/constants';
+import { StyleSheet } from 'react-native';
+import { FlatList } from 'react-native';
+import { IEnvironments, IPlants } from '@/interfaces';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   flex: 1;
 `;
 
 export const HeaderWapper = styled.View`
-  padding-horizontal: ${layout.scale() * 32}px;
+  padding-horizontal: ${layout.screenPadding}px;
   padding-top: ${layout.scale() * 32}px;
 `;
 
@@ -21,3 +25,37 @@ export const Title = styled(SubTitle)`
   margin-top: ${layout.scale() * 40}px;
   font-family: ${fonts.medium};
 `;
+
+const styles = StyleSheet.create({
+  environmentsList: {
+    marginVertical: layout.scale() * 24,
+    paddingBottom: layout.scale() * 5,
+    paddingHorizontal: layout.screenPadding,
+    height: layout.scale() * 40,
+    justifyContent: 'center'
+  },
+  plantsList: {
+    paddingBottom: getBottomSpace()
+  },
+  plantListColumn: {
+    justifyContent: 'space-between',
+    paddingVertical: 10
+  }
+});
+
+export const PlantsListWapper = styled.View`
+  padding-horizontal: ${layout.screenPadding}px;
+  justify-content: center;
+  flex: 1;
+`;
+
+export const PlantsList = styled(FlatList as new () => FlatList<IPlants>).attrs(() => ({
+  contentContainerStyle: styles.plantsList,
+  columnWrapperStyle: styles.plantListColumn
+}))`
+  flex: 1;
+`;
+
+export const EnvironmentsList = styled(FlatList as new () => FlatList<IEnvironments>).attrs(() => ({
+  contentContainerStyle: styles.environmentsList
+}))``;
