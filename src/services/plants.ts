b049d4a9a9cd038102dc45880d4b3c159795866a
João Manoel Neto <joaaomanooel@gmail.com>
2021-05-21 14:30:57 -0300

    update plants service

--- src/services/plants.ts
+++ src/services/plants.ts
@@ -3,7 +3,7 @@ import { IServicesGet } from '@/interfaces';
 
 const baseURL = '/plants';
 
-const getAll = ({ sort = 'name', order = 'asc' }: IServicesGet) =>
-  api.get(`${baseURL}?_sort=${sort}&order=${order}`);
+const getAll = ({ sort = 'name', order = 'asc', page = 1, limit = 8 }: IServicesGet) =>
+  api.get(`${baseURL}?_sort=${sort}&order=${order}&_page=${page}&_limit=${limit}`);
 
 export default { getAll };
